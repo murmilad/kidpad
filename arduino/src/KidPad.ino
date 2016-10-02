8312fe11345301b9db5b7b2fa28719cf251a9eb2
alex <murmilad@gmail.com>
2016-10-02 23:33:46 +0300

    Add math door

--- arduino/src/KidPad.ino
+++ arduino/src/KidPad.ino
@@ -6,10 +6,10 @@
 #define CS_DI 10
 /* Digital pin number for the displays data/command pin */
 #define DC_DI 9
-/* Digital pin number for the displays reset pin */
+/* Digital pin number for the displays -reset pin */
 #define RST_DI 8
 
-
+//D0 D1 CS A0 RESET
 U8GLIB_SSD1306_128X64 u8g(7, 6, 5, 3, 2);
 
 // ROTARY
@@ -20,8 +20,12 @@ int lastState = LOW;
 int trueState = LOW;
 long lastStateChangeTime = 0;
 int cleared = 0;
+short digit_1;
+short digit_2;
+short operation = 0;
 
 #define ROTARY_IN 4
+#define DOOR_1 8
 #define FINISHED_AFTER_MS 100
 #define DEBOUNCE_DELAY 10
 
@@ -47,15 +51,20 @@ void setup()
   // ROTARY
   
   pinMode(ROTARY_IN, INPUT);
+
+  // DOOR
+  randomSeed(analogRead(0));
+  pinMode(DOOR_1, OUTPUT);
  
 }
 
 void loop() 
 {
-
+ 
   // LCD
 
 
+         
   // ROTARY
  
   int reading = digitalRead(ROTARY_IN);
@@ -75,13 +84,56 @@ void loop()
         // graphic commands to redraw the complete screen should be placed here  
         u8g.setFont(u8g_font_osr35);
         //u8g.setFont(u8g_font_osb21);
-        u8g.drawStr( 50, 60 , charBufVar);
+        u8g.drawStr( 50, 52 , charBufVar);
+        
+       
       } while( u8g.nextPage() );
 
+      delay(400);
+      if (
+        (operation == 1 && ((digit_1 + digit_2) == (count % 10)))
+        || (operation == 2 && ((digit_1 - digit_2) == (count % 10)))
+      ) {
+        digitalWrite(DOOR_1, HIGH);
+        u8g.firstPage();  
+        do {
+          char charBufVar[150];
+          String(count % 10).toCharArray(charBufVar, 150);
+          String("Open").toCharArray(charBufVar, 150);
+          u8g.drawStr( 10, 52 , charBufVar);
+        } while( u8g.nextPage() );
+        delay(3000);
+        digitalWrite(DOOR_1, LOW);
+      }
+
+      operation = 0;
       needToPrint = 0;
       count = 0;
       cleared = 0;
-    }
+    } else if (operation == 0) {
+      operation = random(1,3);
+      String op = "";
+      if (operation == 1) {
+        digit_1 = random(9);
+        digit_2 = random(9 - digit_1);
+        op = "+";
+      } else if (operation == 2) {
+        digit_1 = random(9);
+        digit_2 = random(digit_1);
+        op = "-";
+      }
+      
+      
+      u8g.firstPage();  
+      do {
+        char charBufOperation[150];
+        (String(digit_1) + op +  String(digit_2)).toCharArray(charBufOperation, 150);
+        // graphic commands to redraw the complete screen should be placed here  
+         u8g.setFont(u8g_font_osr35);
+        //u8g.setFont(u8g_font_osb21);
+        u8g.drawStr( 15, 52 , charBufOperation);
+      } while( u8g.nextPage() );
+   }
   } 
 
   if (reading != lastState) {
@@ -107,6 +159,5 @@ void loop()
 
   lastState = reading;   
 }
-
 
 
